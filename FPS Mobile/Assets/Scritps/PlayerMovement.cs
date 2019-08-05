63ab35c562a2a293f21548035e94d949c7f2c9c8
aaveshmistry <48035013+aaveshmistry@users.noreply.github.com>
2019-08-05 14:20:15 +1000

    Update

---
+++ FPS Mobile/Assets/Scritps/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float walkSpeed = 6f;
+    public float jumpHeight = 6f;
+
+
+
+    void Update()
+    {
+            
+    }
+
+}
